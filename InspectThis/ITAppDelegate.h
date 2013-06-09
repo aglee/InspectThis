eeac3174bb76de25893f22ecdf77d704f5d41efb
Andy Lee <aglee@mac.com>
2013-06-09 11:31:56 -0400

    Initial Commit

---
+++ InspectThis/ITAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ITAppDelegate.h
+//  InspectThis
+//
+//  Created by Andy Lee on 6/9/13.
+//  Copyright (c) 2013 Andy Lee. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ITAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ InspectThis/ITAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  ITAppDelegate.m
+//  InspectThis
+//
+//  Created by Andy Lee on 6/9/13.
+//  Copyright (c) 2013 Andy Lee. All rights reserved.
+//
+
+#import "ITAppDelegate.h"
+
+@implementation ITAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
